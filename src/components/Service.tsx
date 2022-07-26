ee9d25760674487e0e4eb63fa0b7d4ac13c91180
woojeong kong <coolstevevc@gmail.com>
2022-07-26 20:00:34 +0900

    feat: Add service css styles

--- src/components/Service.tsx
+++ src/components/Service.tsx
@@ -1,10 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 
 import styled from "styled-components";
 
-import icon01 from "../assets/icon01.png";
 import arrowicon from "../assets/arrowicon.png";
-import mainBusiness from "../assets/main_business_img01.jpeg";
+import business01 from "../assets/main_business_img01.jpeg";
+import business02 from "../assets/main_business_img02.jpeg";
+import business03 from "../assets/main_business_img03.jpeg";
+import business04 from "../assets/main_business_img04.jpeg";
+import icon03 from "../assets/icon03.png";
+import icon04 from "../assets/icon04.png";
+import icon06 from "../assets/icon06.png";
+import icon07 from "../assets/icon07.png";
+
+interface Info {
+  [key: string]: {
+    name: string;
+    title: string[];
+    comment: string[];
+    image: string;
+  };
+}
+
+const businessInfo: Info = {
+  distribution: {
+    name: "물류",
+    title: ["상품의 관리,", "보관부터 배송까지", "완벽하게"],
+    comment: [
+      "이커머스 운영에 최적화된 풀필먼트 센터와",
+      "새벽배송망을 보유한 팀프레시의",
+      "이커머스 전문 물류 서비스를 제공받으세요.",
+    ],
+    image: business01,
+  },
+  circulation: {
+    name: "유통",
+    title: ["식자재 제안,", "업장 운영 형태에 맞는", "최적의 형태로"],
+    comment: [
+      "주기적인 단가 관리 시스템을 통해",
+      "합리적인 가격의 상품을 제공하고,",
+      "자체 물류 역량으로 상품을 안정적으로 공급하고 있습니다.",
+    ],
+    image: business02,
+  },
+  franchise: {
+    name: "프렌차이즈",
+    title: ["외식 창업,", "적은 자본으로", "빠르고 간편하게"],
+    comment: [
+      "외식 전문 브랜드 팀프에프앤비가",
+      "소형, 소자본으로도 쉽고 편리하게",
+      "점포를 운영할 수 있도록",
+      "가맹 패키지를 제안해드립니다.",
+    ],
+    image: business03,
+  },
+  insurance: {
+    name: "보험",
+    title: [
+      "기업 대상 전문 보험,",
+      "상해보험부터 재산종합보험까지",
+      "똑똑하게",
+    ],
+    comment: [
+      "기업 전문 보험 GA와 만나",
+      "팀프인슈어런스가 탄생했습니다.",
+      "기업 맞춤형, 최적의 보험 상품을 추천받아보세요.",
+    ],
+    image: business04,
+  },
+};
 
 const ServiceWrapper = styled.div`
   position: relative;
@@ -56,6 +119,11 @@ const ServiceWrapper = styled.div`
         .on {
           color: #fff;
           background: #02204b;
+
+          img {
+            filter: invert(100%) sepia(5%) saturate(0%) hue-rotate(244deg)
+              brightness(104%) contrast(104%);
+          }
         }
 
         li {
@@ -137,6 +205,13 @@ const ServiceWrapper = styled.div`
 `;
 
 const Service: React.FC = () => {
+  const [selectedBusiness, setSeletedBusiness] =
+    useState<string>("distribution");
+
+  function handleClick(businessType: string) {
+    setSeletedBusiness(businessType);
+  }
+
   return (
     <ServiceWrapper>
       <div className="title">
@@ -147,46 +222,59 @@ const Service: React.FC = () => {
       <div className="service-tab-container">
         <div className="service-tab">
           <ul>
-            <li>
-              <img src={icon01} />
-              물류
-            </li>
-            <li>
-              <img src={icon01} />
-              유통
-            </li>
-            <li>
-              <img src={icon01} />
-              프렌차이즈
-            </li>
-            <li>
-              <img src={icon01} />
-              보험
-            </li>
+            {Object.keys(businessInfo).map((value, index) => {
+              return (
+                <li
+                  key={index}
+                  className={selectedBusiness === value ? "on" : ""}
+                  onClick={() => handleClick(value)}
+                >
+                  <img src={icon03} />
+                  {businessInfo[value].name}
+                </li>
+              );
+            })}
           </ul>
         </div>
         <div className="service-contents">
           <div className="text-box">
             <div className="title">
-              상품의 관리,
-              <br />
-              보관부터 배송까지
-              <br />
-              완벽하게
+              {businessInfo[selectedBusiness].title.map((value, index) => {
+                if (index === businessInfo[selectedBusiness].title.length - 1) {
+                  return value;
+                }
+
+                return (
+                  <>
+                    {value}
+                    <br />
+                  </>
+                );
+              })}
             </div>
             <div className="comment">
-              이커머스 운영에 최적화된 풀필먼트 센터와
-              <br />
-              새벽배송망을 보유한 팀프레시의
-              <br />
-              이커머스 전문 물류 서비스를 제공받으세요.
+              {businessInfo[selectedBusiness].comment.map((value, index) => {
+                if (
+                  index ===
+                  businessInfo[selectedBusiness].comment.length - 1
+                ) {
+                  return value;
+                }
+
+                return (
+                  <>
+                    {value}
+                    <br />
+                  </>
+                );
+              })}
             </div>
             <div className="link">
               <img src={arrowicon} />
             </div>
           </div>
           <div className="image-box">
-            <img src={mainBusiness} />
+            <img src={businessInfo[selectedBusiness].image} />
           </div>
         </div>
       </div>
