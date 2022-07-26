00b912f0d0d081188a1e180b42279488c90e2e7a
woojeong kong <coolstevevc@gmail.com>
2022-07-26 20:03:05 +0900

    feat: Add point css styles

--- src/components/Point.tsx
+++ src/components/Point.tsx
@@ -1,6 +1,45 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
 
 import styled from "styled-components";
+
+import image01 from "../assets/main_pointimg01.jpeg";
+import image02 from "../assets/main_pointimg02.jpeg";
+import image03 from "../assets/main_pointimg03.jpeg";
+import image04 from "../assets/main_pointimg04.jpeg";
+
+interface Info {
+  [key: string]: {
+    title: string;
+    comments: string[];
+    image: string;
+  };
+}
+
+const pointInfo: Info = {
+  tms: {
+    title: "특허 보유 TMS",
+    comments: [
+      "새벽배송 현황 추적에 최적화된 배차 및 고객 주문관리 시스템",
+      "* TMS : Transportation Management System",
+    ],
+    image: image01,
+  },
+  morning: {
+    title: "국내 1위 새벽배송",
+    comments: ["대한민국 1위 안정된 새벽배송망 구축"],
+    image: image02,
+  },
+  control: {
+    title: "24시간 관제",
+    comments: ["24시간 현장 관제를 통해 상품을 안전하게 배송 완료"],
+    image: image03,
+  },
+  license: {
+    title: "라이선스 보유 차량망",
+    comments: ["합법적으로 허가 받은 차량만을 섭외해 운영"],
+    image: image04,
+  },
+};
 
 const PointWrapper = styled.div`
   position: relative;
@@ -13,6 +52,10 @@ const PointWrapper = styled.div`
     rgba(0, 0, 0, 0) 0% 50%,
     rgba(0, 0, 0, 0.05) 50% 100%
   );
+
+  * {
+    transition: all 0.4s ease;
+  }
 
   .point-container {
     width: 1210px;
@@ -49,6 +92,13 @@ const PointWrapper = styled.div`
         background: #000;
         overflow: hidden;
 
+        img {
+          width: 100%;
+          height: 100%;
+          opacity: 0.3;
+          object-fit: cover;
+        }
+
         .text {
           position: absolute;
           bottom: 0;
@@ -56,47 +106,65 @@ const PointWrapper = styled.div`
           width: 100%;
           padding: 20px;
           z-index: 1;
+          background: linear-gradient(
+            to bottom,
+            rgba(0, 0, 0, 0),
+            rgba(0, 0, 0, 1)
+          );
+          padding: 40px 40px 30px 40px;
 
           .title {
             width: 100%;
             font-family: "Pretendard";
-            font-size: 25px;
+            font-size: 23px;
             font-weight: 500;
             line-height: 25px;
             color: #fff;
           }
 
           .comments {
-            width: 0;
-            height: 0;
             font-family: "Pretendard";
             font-size: 17px;
             font-weight: 300;
             line-height: 20px;
             color: #fff;
-            opacity: 0;
             border-top: 1px solid #fff;
             padding: 10px 0 0 0;
             margin: 10px 0 0 0;
             overflow: hidden;
+            opacity: 0;
+            width: 0;
+            height: 0;
           }
-        }
-
-        .image {
-          width: 100%;
-          height: 100%;
-          opacity: 0.3;
         }
       }
 
       .active {
         width: 54.7%;
+
+        img {
+          opacity: 1;
+        }
+
+        .text {
+          .title {
+            text-align: left;
+          }
+
+          .comments {
+            width: 100%;
+            height: 60px;
+            opacity: 1;
+          }
+        }
       }
     }
   }
 `;
 
 const Point: React.FC = () => {
+  const [selectedItem, setSelectedItem] = useState<string>("tms");
+
   return (
     <PointWrapper>
       <div className="point-container">
@@ -106,46 +174,38 @@ const Point: React.FC = () => {
           핵심역량
         </div>
         <div className="point-box">
-          <div className="contents active">
-            <div className="text">
-              <div className="title">특허 보유 TMS</div>
-              <div className="comments">
-                새벽배송 현황 추적에 최적화된 배차 및 고객 주문관리 시스템
-                <br />* TMS : Transportation Management System
+          {Object.keys(pointInfo).map((item) => {
+            return (
+              <div
+                key={item}
+                className={
+                  selectedItem === item ? "contents active" : "contents"
+                }
+                onMouseOver={() => {
+                  setSelectedItem(item);
+                }}
+              >
+                <div className="text">
+                  <div className="title">{pointInfo[item].title}</div>
+                  <div className="comments">
+                    {pointInfo[item].comments.map((value, index) => {
+                      if (index === pointInfo[item].comments.length - 1) {
+                        return value;
+                      }
+
+                      return (
+                        <Fragment key={index}>
+                          {value}
+                          <br />
+                        </Fragment>
+                      );
+                    })}
+                  </div>
+                </div>
+                <img src={pointInfo[item].image} />
               </div>
-            </div>
-            <div className="image"></div>
-          </div>
-          <div className="contents">
-            <div className="text">
-              <div className="title">특허 보유 TMS</div>
-              <div className="comments">
-                새벽배송 현황 추적에 최적화된 배차 및 고객 주문관리 시스템
-                <br />* TMS : Transportation Management System
-              </div>
-            </div>
-            <div className="image"></div>
-          </div>
-          <div className="contents">
-            <div className="text">
-              <div className="title">특허 보유 TMS</div>
-              <div className="comments">
-                새벽배송 현황 추적에 최적화된 배차 및 고객 주문관리 시스템
-                <br />* TMS : Transportation Management System
-              </div>
-            </div>
-            <div className="image"></div>
-          </div>
-          <div className="contents">
-            <div className="text">
-              <div className="title">특허 보유 TMS</div>
-              <div className="comments">
-                새벽배송 현황 추적에 최적화된 배차 및 고객 주문관리 시스템
-                <br />* TMS : Transportation Management System
-              </div>
-            </div>
-            <div className="image"></div>
-          </div>
+            );
+          })}
         </div>
       </div>
     </PointWrapper>
